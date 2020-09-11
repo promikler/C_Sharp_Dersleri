c4ace8e88f3f820e53cc2b707e0c12a4ce0ecd4a
Promikler <omer_ocak_1997@hotmail.com>
2020-09-11 14:00:26 +0300

    Promikler 11.09.2020
    Diziler ve çok boyutlu diziler kullanım şekilleri.

---
+++ Cok_Boyutlu_Diziler_Multidimensional_Arrays/Cok_Boyutlu_Diziler_Multidimensional_Arrays/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cok_Boyutlu_Diziler_Multidimensional_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Çok Boyutlu Diziler-1
+            string[,] regions = new string[5, 3]
+            {
+                { "istanbul","izmit","balıkesir"},
+                  { "ankara","konya","yozgat"},
+                  { "antalya","adana","mersin"},
+                  { "rize","trabzon","samsun"},
+                  { "izmir","mugla","manisa"},
+            };
+            for (int i = 0; i <=regions.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <= regions.GetUpperBound(1); j++)
+                {
+                    Console.WriteLine(regions[i, j]);
+                }
+                Console.WriteLine("**************************");
+            }
+            Console.ReadLine();
+            //-------------------------------------------------
+        }
+    }
+}
---
+++ Diziler_Arrays/Diziler_Arrays/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diziler_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Diziler kullanım şekli -1
+            string[] ogrenciler = new string[3];
+            ogrenciler[0] = "omer";
+            ogrenciler[1] = "ahmet";
+            ogrenciler[2] = "mert";
+            foreach (var ogrenci in ogrenciler)
+            {
+                Console.WriteLine(ogrenci);
+            }
+            //-----------------------------
+            // Diziler kullanım şekli -2
+            string[] ogrenciler2 = { "omer","mert","ahmet" };
+            foreach (var ogrenci2 in ogrenciler2)
+            {
+                Console.WriteLine(ogrenci2);
+            }
+            //-----------------------------
+            // Diziler kullanım şekli -3
+            string[] ogrenciler3 =new [] { "omer","mert","ahmet" };
+            foreach (var ogrenci3 in ogrenciler3)
+            {
+                Console.WriteLine(ogrenci3);
+            }
+            Console.ReadLine();
+            //-----------------------------
+        }
+    }
+}
